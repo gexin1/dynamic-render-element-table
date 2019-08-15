ba2d8e9a103cef979b0560b50d8aba7c1a220f45
葛欣 <382599512@qq.com>
2019-08-15 11:46:09 +0800

    fix表格布局不能对齐

--- components/DynamicTable/index.vue
+++ components/DynamicTable/index.vue
@@ -1,44 +1,63 @@
 <template>
-    <el-table :data="data" v-bind="attrs" v-on="event">
-        <column-plus
-            v-for="(item, index) in columns"
-            :key="index"
-            :attrs="item"
-        ></column-plus>
-    </el-table>
+  <el-table :data="data" v-bind="attrs" v-on="event" ref="el-table">
+    <template v-for="(item, index) in columns">
+      <column-plus v-if="!item.hidden" :key="index" :attrs="item"></column-plus>
+    </template>
+  </el-table>
 </template>
 
 <script>
 import ColumnPlus from './ColumnPlus';
 export default {
-    props: {
-        data: {
-            type: Array,
-            required: true
-        },
-        columns: {
-            type: Array,
-            required: true
-        },
-        attrs: {
-            type: Object,
-            default: () => ({})
-        },
-        event: {
-            type: Object,
-            default: () => ({})
-        }
+  props: {
+    data: {
+      type: Array,
+      required: true
     },
-    computed: {
-        _attrs() {
-            //默认table 参数
-            const defaultParams = {};
-            return Object.assign(this.attrs, defaultParams);
-        }
+    columns: {
+      type: Array,
+      required: true
     },
-    components: {
-        ColumnPlus
+    attrs: {
+      type: Object,
+      default: () => ({})
+    },
+    event: {
+      type: Object,
+      default: () => ({})
     }
+  },
+  computed: {
+    _attrs() {
+      //默认table 参数
+      const defaultParams = {};
+      return Object.assign(defaultParams, this.attrs);
+    }
+  },
+  components: {
+    ColumnPlus
+  },
+  methods: {
+    /**
+     * 使用 el-table的方法
+     */
+    useTableFunc(FuncName, ...params) {
+      if (!this.$refs['el-table']) {
+        return console.warn('访问不到el-table');
+      }
+      if (!FuncName) {
+        return console.error('请传入tabel方法名字');
+      }
+      this.$refs['el-table'][FuncName](params[0]);
+    }
+  },
+  watch: {
+    data() {
+      this.$nextTick(() => {
+        this.useTableFunc('doLayout');
+      });
+    }
+  }
 };
 </script>
 
